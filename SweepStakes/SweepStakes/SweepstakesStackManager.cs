e49bd11eb40b77ff9708a881c34fc571779ee594
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-10-11 19:14:35 -0500

    Implemented Stack Data structure in SweepstakesStackManager class

--- SweepStakes/SweepStakes/SweepstakesStackManager.cs
+++ SweepStakes/SweepStakes/SweepstakesStackManager.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace SweepStakes
 {
-    class SweepstakesStackManager
+    class SweepstakesStackManager:ISweepstakesManager
     {
+        Stack<SweepStakes> sweepsStack = new Stack<SweepStakes>();
+        public void InsertSweepstakes(SweepStakes sweepstakes)
+        {
+            sweepsStack.Push(sweepstakes);
+        }
+        public SweepStakes GetNextSweepstakesWinner()
+        {
+            return sweepsStack.Pop();
+        }
     }
 }
